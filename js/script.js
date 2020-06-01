107fcb0c61d5c100d1bd10a4bbb1a6b862b2a3b1
Adel <adelpakrad@gmail.com>
2020-05-31 22:17:22 -0700

    improving menu and more videos

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// setTimeout(function(){
+//     document.getElementById("slider-video-1").play();
+//   }, 5000);
--- script.js
+++
@@ -1,3 +0,0 @@
-// setTimeout(function(){
-//     document.getElementById("slider-video-1").play();
-//   }, 5000);
